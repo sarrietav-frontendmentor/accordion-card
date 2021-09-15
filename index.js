2771f771566318e49281e6e59b50820aae28ffe3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-09-15 01:11:57 -0500

    feat(js): close other accordions when opening one

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const accordions = document.querySelectorAll('.accordion');
+
 accordions.forEach((element) => {
   element.addEventListener('click', () => {
     if (element.classList.contains('accordion--open')) {
-      element.classList.remove('accordion--open');
+      closeAccordion(element);
     } else {
-      element.classList.add('accordion--open');
+      closeAccordions();
+      openAccordion(element);
     }
   });
 });
+
+function openAccordion(accordion) {
+  accordion.classList.add('accordion--open');
+}
+
+function closeAccordion(accordion) {
+  accordion.classList.remove('accordion--open');
+}
+
+function closeAccordions() {
+  accordions.forEach((element) => {
+    closeAccordion(element);
+  });
+}
